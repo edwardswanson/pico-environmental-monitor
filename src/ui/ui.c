38d8912c7bbe537fe9b52cf2ab7a0d38090dc49b
swanalex <swanalex@oregonstate.edu>
2026-02-22 19:30:48 -0500

    Add #defines for temp levels (in °F for readability)

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -16,6 +16,14 @@
 
 #define BLACK 0x000000
 
+// Temerature Levels (in degrees Fahrenheit):
+#define TEMP_VERY_COLD 16
+#define TEMP_COLD 32
+#define TEMP_COOL 48
+#define TEMP_MILD 64
+#define TEMP_HOT 80
+#define TEMP_VERY_HOT 96
+
 /**
  * @brief Updates the LED array to display new humidity
  * @param humidity The new humidity to set
@@ -77,17 +85,18 @@ static uint8_t curr_led_pattern = 2;  // 1 = light all same color, 2 = progressi
 
 /**
  * @brief Get WS2812 color from temperature value
- * @param temp Temperature in Celsius
+ * @param temp Temperature in Fahrenheit
  * @return 32-bit GRB color value (0xGGRRBB format)
  * @private
  */
 static uint32_t get_temp_color(float temp) {
-    return temp < -10 ? PURPLE :
-           temp < -2 ? BLUE  :
-           temp < 6 ? TEAL  :
-           temp < 14 ? GREEN :
-           temp < 22 ? YELLOW :
-           temp < 29 ? ORANGE : RED;
+    if (temp < TEMP_VERY_COLD)      return PURPLE;
+    else if (temp < TEMP_COLD)      return BLUE;
+    else if (temp < TEMP_COOL)      return TEAL;
+    else if (temp < TEMP_MILD)      return GREEN;
+    else if (temp < TEMP_HOT)       return YELLOW;
+    else if (temp < TEMP_VERY_HOT)  return ORANGE;
+    else                            return RED;
 }
 
 /**
@@ -97,30 +106,31 @@ static uint32_t get_temp_color(float temp) {
  * @private
  */
 static uint8_t get_num_leds_to_light(float temp) {
-    return temp < -10 ? 2 :
-           temp < -2 ? 3  :
-           temp < 6 ? 4  :
-           temp < 14 ? 5 :
-           temp < 22 ? 6 :
-           temp < 29 ? 7 : 8;
+    if (temp < TEMP_VERY_COLD)      return 2;
+    else if (temp < TEMP_COLD)      return 3;
+    else if (temp < TEMP_COOL)      return 4;
+    else if (temp < TEMP_MILD)      return 5;
+    else if (temp < TEMP_HOT)       return 6;
+    else if (temp < TEMP_VERY_HOT)  return 7;
+    else                            return 8;
 }
 
 /**
  * @brief 
  * Pattern 1: All LEDs same color based on temperature
  * Pattern 2: Progressive fill based on temperature
- * @param temp Temperature in Celsius
+ * @param temp Temperature in Fahrenheit
  * 
  * Progressive fill chart:
  * | Temperature | LEDs | Color  |
  * |-------------|------|--------|
- * | < -10°C     | 2    | Purple |
- * | -10 to -2°C | 3    | Blue   |
- * | -2 to 6°C   | 4    | Teal   |
- * | 6 to 14°C   | 5    | Green  |
- * | 14 to 22°C  | 6    | Yellow |
- * | 22 to 29°C  | 7    | Orange |
- * | ≥ 29°C      | 8    | Red    |
+ * | < 16°F      | 2    | Purple | very cold
+ * | 17 to 32°F  | 3    | Blue   | cold 
+ * | 33 to 48°F  | 4    | Teal   | cool
+ * | 49 to 64°F  | 5    | Green  | mild
+ * | 65 to 80°F  | 6    | Yellow | hot
+ * | 81 to 96°F  | 7    | Orange | very hot
+ * | ≥ 96°F      | 8    | Red    | extreme heat *WARNING*
  */
 static void update_led_strip(float temp) {
     uint32_t color = get_temp_color(temp);
